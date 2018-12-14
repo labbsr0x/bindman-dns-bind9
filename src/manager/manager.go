6107c25513e9b973709d54fc9e836deacd05b933
Abilio Esteves <abilio.esteves@ibm.com>
2018-12-14 11:43:04 -0200

    sync over folder

--- src/manager/manager.go
+++ src/manager/manager.go
@@ -2,9 +2,12 @@ package manager
 
 import (
 	"encoding/json"
+	"fmt"
 	"os"
+	"path/filepath"
 	"strconv"
 	"strings"
+	"sync"
 	"time"
 
 	"github.com/Sirupsen/logrus"
@@ -19,6 +22,7 @@ type Bind9Manager struct {
 	TTL          int
 	RemovalDelay time.Duration
 	NSUpdate     *nsupdate.NSUpdate
+	Door         *sync.RWMutex
 }
 
 // New creates a new Bind9Manager
@@ -35,6 +39,7 @@ func New() (result *Bind9Manager) {
 		TTL:          3600,
 		RemovalDelay: 10 * time.Minute,
 		NSUpdate:     nsu,
+		Door:         new(sync.RWMutex),
 	}
 
 	// get ttl from env
@@ -52,14 +57,32 @@ func New() (result *Bind9Manager) {
 
 // GetDNSRecords retrieves all the dns records being managed
 func (m *Bind9Manager) GetDNSRecords() ([]hookTypes.DNSRecord, error) {
+	m.Door.RLock()
+	defer m.Door.RUnlock()
+
 	toReturn := []hookTypes.DNSRecord{}
-	// TODO: navigate data subfolders
+
+	err := filepath.Walk(BasePath, func(path string, info os.FileInfo, err error) error {
+		if strings.HasSuffix(path, Extension) {
+			r, _ := m.GetDNSRecord(m.getRecordName(info.Name()))
+			toReturn = append(toReturn, *r)
+		}
+		return nil
+	})
+
+	if err != nil {
+		return nil, fmt.Errorf("Unable to read the files of the %v folder. Err: %v", BasePath, err)
+	}
+
 	return toReturn, nil
 }
 
 // GetDNSRecord retrieves the dns record identified by name
 func (m *Bind9Manager) GetDNSRecord(name string) (*hookTypes.DNSRecord, error) {
-	r, _ := m.DNSRecords.Read(name)
+	m.Door.RLock()
+	defer m.Door.RUnlock()
+
+	r, _ := m.DNSRecords.Read(m.getRecordFileName(name))
 	toReturn := &hookTypes.DNSRecord{}
 	err := json.Unmarshal(r, toReturn)
 	if err == nil {
@@ -73,8 +96,11 @@ func (m *Bind9Manager) AddDNSRecord(record hookTypes.DNSRecord) (bool, error) {
 	succ, err := m.NSUpdate.AddRR(record.Name, record.Type, record.Value, m.TTL)
 	if succ {
 		r, _ := json.Marshal(record)
-		m.DNSRecords.Write(record.Name, r)
 
+		m.Door.Lock()
+		defer m.Door.Unlock()
+
+		m.DNSRecords.Write(m.getRecordFileName(record.Name), r)
 		return true, nil
 	}
 	return false, err
