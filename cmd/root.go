6b02efcd0cde4625ace8752b3bda8d0b0b981068
Werberson Silva <werberson.silva@gmail.com>
2020-03-28 13:05:09 -0300

    add feature to config from file

--- cmd/root.go
+++ cmd/root.go
@@ -1,12 +1,16 @@
 package cmd
 
 import (
+	"fmt"
 	"os"
 	"strings"
 
+	"github.com/mitchellh/go-homedir"
 	"github.com/spf13/cobra"
 	"github.com/spf13/viper"
 )
+
+var cfgFile string
 
 // rootCmd represents the base command when called without any sub-commands
 var rootCmd = &cobra.Command{
@@ -25,10 +29,32 @@ func Execute() {
 
 func init() {
 	cobra.OnInitialize(initConfig)
+	rootCmd.PersistentFlags().StringVar(&cfgFile, "config", "", "config file (default is $HOME/.bindman-dns-bind9.yaml)")
 }
 
 // initConfig reads ENV variables if set.
 func initConfig() {
+	if cfgFile != "" {
+		// Use config file from the flag.
+		viper.SetConfigFile(cfgFile)
+	} else {
+		// Find home directory.
+		home, err := homedir.Dir()
+		if err != nil {
+			fmt.Println(err)
+			os.Exit(1)
+		}
+
+		// Search config in home directory with name ".service" (without extension).
+		viper.AddConfigPath(home)
+		viper.SetConfigName(".bindman-dns-bind9")
+	}
+
+	// If a config file is found, read it in.
+	if err := viper.ReadInConfig(); err == nil {
+		fmt.Println("Using config file:", viper.ConfigFileUsed())
+	}
+
 	viper.SetEnvPrefix("BINDMAN")
 	viper.SetEnvKeyReplacer(strings.NewReplacer("-", "_", ".", "_"))
 	viper.AutomaticEnv() // read in environment variables that match
